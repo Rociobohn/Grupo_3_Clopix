3f6739a4e474f71a18674ffd724419daf305ab58
S_Ariel <xaxwtf@gmail.com>
2021-12-20 01:05:49 -0300

    rutas arregladas, ajuste del carrito y detalle de compra, faltan los controllers

--- proyecto-Clopix/src/controllers/productoCrl.js
+++ proyecto-Clopix/src/controllers/productoCrl.js
@@ -1,3 +1,7 @@
+const req = require('express/lib/request');
+const gArchivoJson=require('../model/jsonDataBase');
+let vProductos=gArchivoJson('product');
+let Detalle=gArchivoJson('listaComprasTest');
 const producto={
     alta:(req, res)=>{
         res.render('Products/AltaProducto.ejs');
@@ -9,10 +13,14 @@ const producto={
         res.render('Products/ModificarProducto');
     },
     Catalogo:(req, res)=>{
-        res.render('Products/productDetail');
+        res.render('Products/productDetail',{producto:vProductos.readFile()});
     },
     Carrito:(req, res)=>{
-        res.render('Products/CarritoCompras');
+        res.render('Products/CarritoCompras',{ DetalleCompra: Detalle.readFile()});
+    },
+    Detalle:(req, res)=>{
+        let id=req.params.id;
+        res.render('Products/unProducto',{ producto: vProductos.find(id)})
     }
     
 }

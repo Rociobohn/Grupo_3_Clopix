8435379d9ce3fad5fe307dba9cc14150ad0b2edb
S_Ariel <xaxwtf@gmail.com>
2022-02-24 21:57:26 -0300

    conectado a la base de datos

--- proyecto-Clopix/database/config/config.js
+++ proyecto-Clopix/database/config/config.js
@@ -1,11 +1,20 @@
-module.exports=
+require('dotenv').config()
+module.exports =
 {
-  "development": {
+    "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+},
+    "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT
-  }
+}
 }

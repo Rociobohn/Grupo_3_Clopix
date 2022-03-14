4d87d5a31ad54d14f6bd6fc31ca73772d53615d7
MicaBellusci <mica9111@gmail.com>
2022-03-14 10:22:49 -0300

    errores

--- proyecto-Clopix/public/scripts/login.js
+++ proyecto-Clopix/public/scripts/login.js
@@ -8,7 +8,13 @@ loginForm.addEventListener('submit', function(event){
     //validación usuario
 loginForm.user.addEventListener('keydown', userValidate);
 
-
+function userValidate(){
+    if(loginForm.user.value < 2 ){
+        loginForm.user.style.borderColor= 'red';
+    }else {
+        loginForm.user.style.borderColor= 'inherit';
+    }
+} 
 
     //validación de contraseña
 if (this.password.value.length < 8){

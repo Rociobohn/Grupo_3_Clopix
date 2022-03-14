111f8df9fb90b306790aee32e79b2643fcddbe66
MicaBellusci <mica9111@gmail.com>
2022-03-14 10:06:04 -0300

    validación de contraseña

--- proyecto-Clopix/public/scripts/login.js
+++ proyecto-Clopix/public/scripts/login.js
@@ -1,5 +1,14 @@
 const loginForm = document.forms['loginForm'];
 
+loginForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    let errors= false;
 
+    //validación de contraseña
+if (this.password.value.length < 8){
+    errors= true;
+    document.getElementById('errorPassword').innerHTML='La contraseña debe tener al menos 8 caracteres';
+    } 
+});
 
 <script src="/scripts/login.js"> </script>
